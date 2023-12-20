86b224c014d8f83dd80795d89b0d54a991ba0d43
Arlindo Neto <netonery06@gmail.com>
2023-12-19 22:40:42 -0300

    ChessException

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,9 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
+
 import chess.chessPieces.Rook;
+
 public class ChessMatch {
     private Board board;
 
@@ -11,18 +12,22 @@ public class ChessMatch {
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces(){
+    public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for(int i = 0; i<board.getRows(); i++){
-            for(int j = 0; j < board.getColumns(); j++){
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
-    
-    private void initialSetup(){
-        board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
+
+    private void placeNewPiece(char colum, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(colum, row).toPosition());
+    }
+
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
     }
 
 }
