644c622356ae4ce343d82dbfb6039f00949711bb
Arlindo Neto <netonery06@gmail.com>
2023-12-21 21:55:00 -0300

    FinalCommit

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public class Position {
+
     private int row;
     private int column;
 
@@ -17,16 +18,21 @@ public class Position {
         this.row = row;
     }
 
-    public int getColumn() { // Corrigido o nome do método
+    public int getColumn() {
         return column;
     }
 
-    public void setColumn(int column) { // Corrigido o nome do método
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
         this.column = column;
     }
 
     @Override
     public String toString() {
-        return row + " , " + column;
+        return row + ", " + column;
     }
 }
